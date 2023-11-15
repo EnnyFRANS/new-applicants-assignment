d14686b30cd1ca5a825047ad88d80dd29394a857
EnnyMJE <78366762+EnnyMJE@users.noreply.github.com>
2023-11-15 23:56:42 +0100

    calculator assignment

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-// Add your JavaScript here
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+//I did some online research and browsing for this fonction
+function deleteLastCharacter() {
+    display.value = display.value.slice(0, -1);
+}
+
+//I did some online research and browsing for this fonction
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
